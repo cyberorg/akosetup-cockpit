6207642b1a52f63b095b084b940136701efbf44a
Jigish Gohil <cyberorg@cyberorg.info>
2020-05-10 10:43:39 +0530

    cockpit plugin for Ako AP, https://github.com/RechercheTech/mss/issues/114

---
+++ src/usr/share/cockpit/akosetup/ako.js
@@ -0,0 +1,10 @@
+var AP_SSID = "AKOBOX";
+var AP_PASS = "12345678";
+var AP_IP = "192.168.0.1";
+var NET_CONNECT = "true";
+
+document.getElementById("AP_SSID").value = AP_SSID;
+document.getElementById("AP_PASS").value = AP_PASS;
+document.getElementById("AP_IP").value = AP_IP;
+document.getElementById("NET_CONNECT").value = NET_CONNECT;
+
